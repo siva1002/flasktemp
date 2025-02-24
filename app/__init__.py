c2a79a9709a5efdb6153b909f3cdb02e668124c3
siva1002 <siva2k1002@gmail.com>
2025-02-24 22:33:54 +0530

    changes

--- app/__init__.py
+++ app/__init__.py
@@ -6,5 +6,5 @@ app = Flask(__name__)
 from app import views
 
 
-app.run(debug=True)
+app.run()
 app.config['SECRET_KEY'] =os.environ.get('SECRET_KEY')
---
+++ wsgi.py
@@ -0,0 +1,4 @@
+from app import app
+bind = "0.0.0.0:8080"
+workers = 2
+app=app
